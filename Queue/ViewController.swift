5345becc10948706b1a32b88bb170a79f282d4e9
Dennis Lin <dennis@denniss-macbook-pro.local>
2018-02-27 04:42:28 -0500

    Initial Commit

---
+++ Queue/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Queue
+//
+//  Created by Dennis Lin on 2/27/18.
+//  Copyright © 2018 TungHo Lin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
